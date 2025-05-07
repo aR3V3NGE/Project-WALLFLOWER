763f2a087a12aeed45875477848088f504c8b2ee
JubinJaydeep Vijayakumar <158509739+jubin-vjk@users.noreply.github.com>
2025-05-07 04:17:42 -0400

    works ig idk man sue me

--- pathfinder.cpp
+++ pathfinder.cpp
@@ -10,47 +10,47 @@
 #include <ctime>
 #include <cfloat>
 
-#include <iomanip>
-
 using namespace std;
 
 struct Hold { //stores information about a given hold
         int h, w, a;
 };
 
-struct Node { //nodes for dijkstras
+struct Node { //nodes for Dijkstras
         double weight;
         int holdIndex;
 };
 
 bool output(const double &weight, const vector<int> &route, const map<pair<double, double>, Hold>  &wall, const int &grade, const vector<pair<double, double>> &key) {
-
-        if (grade*100 <= weight && weight <= grade*200+200) {
+        //outputs the path if the weight of the path and the inputted grades match
+        if ((grade*50) <= weight && weight <= (grade*50)+50) {
                 for (size_t i = 0; i < route.size(); i++) {
                         map<pair<double, double>, Hold>::const_iterator it = wall.find(key[route[i]]);
-                        cerr << it->first.first << " " << it->first.second << " " << it->second.h << " " << it->second.w;
+                        //turns the path of hold indexes into the hold information
+                        cout << it->first.first << " " << it->first.second << " " << it->second.h << " " << it->second.w;
                 }
-                return true;
+                return true; //true if route was found, false if not
         }
         return false;
 
 }
 
 void dijkstrater(vector<vector<double>> graph, vector<int> &route, const int &holds, int start, int finish, double &weight) {
-
-        vector<bool> visited (holds, false);
-        vector<double> dist (holds, DBL_MAX);
+        //runs Dijkstra's algorithm to find routes
+        /* Dijsktra's algorithim runs over the indexes of the holds and their weights rather than the holds itself
+         * making it easier to code. The route is a series of indeces, and output is converted from index to hold and printed out
+         */
+        vector<bool> visited (holds, false); //stores visited holds
+        vector<double> dist (holds, DBL_MAX); //stores weight of path
         vector<int> prev(holds, -1);// For path reconstruction
         multimap<double, Node> frontier; //stores nodes to be looped through, sorted by weight
 
         dist[start] = 0;
-
         frontier.insert({0, {0, start}});
 
         while (!frontier.empty()) { //Loops until there are no more nodes to loop trough
 
                 multimap<double, Node>::iterator it = frontier.begin();
-
                 Node current = it->second;
 
                 if (visited[current.holdIndex]) {
@@ -91,8 +91,8 @@ void dijkstrater(vector<vector<double>> graph, vector<int> &route, const int &ho
 }
 
 double distanciator(const map<pair<double, double>, Hold> ::const_iterator &it, const map<pair<double, double>, Hold> ::const_iterator &itr) {
-        //finds distance any two given nodes
-        double y1 = it->first.second + it->second.h/2;
+        //finds distance between any the center of two given nodes
+        double y1 = it->first.second + it->second.h/2; //Euclidean distance formula
         double y2 = itr->first.second + itr->second.h/2;
 
         double x1 = it->first.first - it->second.w/2;
@@ -104,14 +104,15 @@ double distanciator(const map<pair<double, double>, Hold> ::const_iterator &it, 
         return sqrt(y+x);
 }
 
-vector<vector<double>> graphify(const map<pair<double, double>, Hold> &wall, int &holds, vector<pair<double, double>> &key) { //turns the input into an adjacency list
-
+vector<vector<double>> graphify(const map<pair<double, double>, Hold> &wall, int &holds, vector<pair<double, double>> &key) {
+        //turns the input into an adjacency list
         vector<vector<double>> graph (holds, vector<double>(holds));
 
         int i = 0;
         for (map<pair<double, double>, Hold>::const_iterator it = wall.begin(); it != wall.end(); it++) {
+                //turns wall into an adjacency list, mapping the cost of traversal to every edge
                 int j = 0;
-                key.push_back(it->first);
+                key.push_back(it->first); //allows to get hold from hold index
                 for (map<pair<double, double>, Hold>::const_iterator itr = wall.begin(); itr != wall.end(); itr++) {
                         if (it == itr) graph[i][j] = 0;
                         else {
@@ -123,59 +124,61 @@ vector<vector<double>> graphify(const map<pair<double, double>, Hold> &wall, int
                 }
                 i++;
         }
-
         return graph;
 }
 
 void unscrew(vector<vector<double>> &graph, const vector<int> &route, map< pair<double, double>, Hold> &wall, const vector<pair<double, double>> &key, int &holds) {
-
-        int x = route[route.size()/2];
+        //removes a hold from the wall, unscrewing it
+        int x = route[route.size()/2]; //finds a hold in the middle of the existing route
         for (size_t i = 0; i < graph.size(); i++) {
+                //erases a hold from the adjacency list by setting its weight to double max so it won't ever be chosen by Dijkstra's algorithm
                 graph[x][i] = DBL_MAX;
                 graph[i][x] = DBL_MAX;
         }
-
         holds--;
 }
 
-// Main Execution
+
 int main(int argc, char* argv[]) {
 
+        //reads in the holds from the input file
         ifstream fin;
         fin.open(argv[1]);
 
+        //creates the wall, sorting by x and y coordinates
         map<pair<double, double>, Hold> wall;
         int vGrade, holds;
         fin >> vGrade;
         double x, y, h, w, a;
 
         while (fin >> x >> y >> h >> w >> a) {
-
                 wall[{x, y}].h = h;
                 wall[{x, y}].w = w;
                 wall[{x, y}].a = a;
-
         }
 
         fin.close();
         holds = wall.size();
 
+        //creates adjacency list
         vector<pair<double, double>> key;
         vector<vector<double>> graph = graphify(wall, holds, key);
 
+        //chooses random start and finish
         srand(time(0));
         int start = rand() % holds/5  + 4*holds/5;
         int finish = rand() % holds/5;
         bool graded = false;
-        while(!graded) {
 
+        while(!graded && holds > 2) { //runs until adequate route is found or it is determined that no route can be created
+               
                 vector<int> route;
                 double weight = 0;
                 dijkstrater(graph, route, holds, start, finish, weight); //runs dijkstra to get routes
-                graded = output(weight, route, wall, vGrade, key);
+                graded = output(weight, route, wall, vGrade, key); //sees if a grade and weight match and prints the path for that route
                 unscrew(graph, route, wall, key, holds); //removes holds
-
         }
 
-        return 0;
+        if (graded) return 0;
+        else cout << "-1";
 }
